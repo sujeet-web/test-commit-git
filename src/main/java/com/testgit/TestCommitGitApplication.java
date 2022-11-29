cbd2775578e6cb8509f8b044e6b2899d8b1237b2
sujeet-web <sujeetsharma675@gmail.com>
2022-11-29 12:15:54 +0530

    first commit

---
+++ src/main/java/com/testgit/TestCommitGitApplication.java
@@ -0,0 +1,13 @@
+package com.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommitGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCommitGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testgit/TestCommitGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCommitGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
